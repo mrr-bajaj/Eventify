a6d4715b0449702b5f7edd21cbf7fae160e4df21
Shubham Bajaj <mrrbajaj44@gmail.com>
2023-07-03 18:11:18 +0530

    admin dashboard css format

--- client/src/app/components/pages/shared/sidebar/sidebar.component.ts
+++ client/src/app/components/pages/shared/sidebar/sidebar.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
-
-  constructor(private route: Router){}
-
-  openDashboard(){}
-
-  openEvents(){}
-
-  openEmployees(){}
-
-  openAdmins(){}
+export class SidebarComponent{
+  
 }
