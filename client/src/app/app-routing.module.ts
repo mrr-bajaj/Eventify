94ac9680911a5fb3692df6781c5a6662b8525924
Shubham Bajaj <mrrbajaj44@gmail.com>
2023-07-03 07:11:31 +0530

    basic admin dashboard layout

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
 import { SignupComponent } from './components/pages/signup/signup.component';
-import { AdminDashboardComponent } from './components/pages/admin-dashboard/admin-dashboard.component';
+import { AdminComponent } from './components/pages/admin/admin.component';
+import { DashboardComponent } from './components/pages/admin/dashboard/dashboard.component';
+import { EventsComponent } from './components/pages/admin/events/events.component';
+import { EmployeesComponent } from './components/pages/admin/employees/employees.component';
+import { AdminRolesComponent } from './components/pages/admin/admin-roles/admin-roles.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  {path: 'admin', component: AdminDashboardComponent}
+  {
+    path: 'admin', 
+    component: AdminComponent,
+    children: [
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+      {path:'dashboard',component:DashboardComponent},
+      {path:'events',component:EventsComponent},
+      {path:'employees',component:EmployeesComponent},
+      {path: 'admin-roles', component: AdminRolesComponent}
+  ]}
 ];
 
 @NgModule({
