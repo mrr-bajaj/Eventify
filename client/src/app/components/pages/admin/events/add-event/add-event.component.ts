2a916db9c8c7d9ec7b7387bceea1c099351a27f5
Shubham Bajaj <mrrbajaj44@gmail.com>
2023-07-07 19:27:14 +0530

    Upload event form data, genrating unique qrcode,event logo to db

--- client/src/app/components/pages/admin/events/add-event/add-event.component.ts
+++ client/src/app/components/pages/admin/events/add-event/add-event.component.ts
@@ -11,7 +11,7 @@ import { EventsService } from 'src/app/services/events/events.service';
 })
 export class AddEventComponent {
 
-  imageFile: File;
+  imageFile: File | null = null;
   @ViewChild('eventForm') eventForm : NgForm;
   eventData : EventModel = {
     eventName: '',
@@ -24,15 +24,36 @@ export class AddEventComponent {
     eventImage: null
   };
 
-  constructor(private eventsService : EventsService,private router: Router,private route:ActivatedRoute){}
+  constructor(
+    private eventsService: EventsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  onSubmit(eventForm:NgForm) {
-    if(eventForm.valid){
+  onSubmit(eventForm: NgForm) {
+    if (eventForm.valid) {
       this.eventData = eventForm.value;
-      this.eventData.eventImage = this.imageFile;
-      this.eventsService.addEvent(this.eventData);
-      this.onReset();
-      this.router.navigate(['/admin/events'],{relativeTo: this.route})
+      this.eventData.eventDate = new Date(eventForm.value.eventDate);
+      const formData = new FormData();
+      formData.append('eventName', this.eventData.eventName);
+      formData.append('eventDescription', this.eventData.eventDescription);
+      formData.append('eventDate', this.eventData.eventDate.toISOString());
+      formData.append('eventStartTime', this.eventData.eventStartTime);
+      formData.append('eventEndTime', this.eventData.eventEndTime);
+      formData.append('eventLocation', this.eventData.eventLocation);
+      formData.append('eventType', this.eventData.eventType);
+      formData.append('eventImage', this.imageFile);
+      this.eventsService.addEvent(formData).subscribe(
+        (response) => {
+          if (response.message === 'Event added successfully') {
+            this.onReset();
+            this.router.navigate(['/admin/events'], { relativeTo: this.route });
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
   }
 
@@ -40,7 +61,7 @@ export class AddEventComponent {
     this.imageFile = event.target.files[0];
   }
 
-  onReset(){
+  onReset() {
     this.eventForm.reset();
   }
 }
